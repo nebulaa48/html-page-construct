6b777216846c827a13c120bd2981b804f30546e9
Mélanie V <melanie.vaubien@hotmail.com>
2025-11-25 20:29:45 +0100

    Fix  Main Exports

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-import { init } from "./scripts/script.js";
-
-export * as HPCelements from "./scripts/html-elements.js";
-export * as HPCjsonRead from "./scripts/json-read.js";
+import { init } from "./scripts/script";
 
 init();
